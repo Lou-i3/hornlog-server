d18a22c96d9e8895ed76bbc5c07b6b486ccce712
Lou-i3 <louis.rouault@gmail.com>
2022-01-23 19:45:11 +0100

    can use port

--- app/models/index.js
+++ app/models/index.js
@@ -4,6 +4,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
+    port: dbConfig.port,
     operatorsAliases: false,
 
     pool: {
