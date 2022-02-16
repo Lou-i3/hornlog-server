b24512ba679598a9fdff98423fe1f362d2616e42
Lou-i3 <louis.rouault@gmail.com>
2022-02-16 19:25:46 +0100

    moved prisma-project into main folder

--- app/config/auth.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    secret: "app-secret-key",
-    jwtExpiration: 3600, // 1 hour
-    jwtRefreshExpiration: 86400, // 24 hours
-
-    /* for test */
-    // jwtExpiration: 60,          // 1 minute
-    // jwtRefreshExpiration: 120,  // 2 minutes
-};
